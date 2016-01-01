4f690baf38e085ebd67f9b6e6fb84ea78971e58d
Paul Taylor <pftaylor61@gmail.com>
2015-12-31 18:12:39 -0600

    Commit 12312015 01

--- ocws-slider.php
+++ ocws-slider.php
@@ -3,11 +3,11 @@
     Plugin Name: OCWS Slider Plugin
     Description: This is a full featured slider plugin. It is actually a simple implementation of a nivo slideshow into WordPress. It utilizes the nivo slider jQuery code, following a tutorial by Ciprian Turcu. A couple of OCWS custom features have been added. Make sure you include the shortcode [ocwssl-shortcode] in any page where you wish the slider to appear.
     Author: Paul Taylor
-    Version: 0.3.1
+    Version: 0.3.4
     Plugin URI: http://oldcastleweb.com/pws/plugins
     Author URI: http://oldcastleweb.com/pws/about
     License: GPL2
-    GitHub Plugin URI: https://github.com/pftaylor61/ocws-creationcache
+    GitHub Plugin URI: https://github.com/pftaylor61/ocws-slider
     GitHub Branch:     master
 */
 
